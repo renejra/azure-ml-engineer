6d25cc7aa07375eb056d8fc8c5dd60c93c721f07
Rene EON <rene.rivero-arrieta@eon.com>
2022-02-01 17:45:05 +0000

    first run in aml

--- capstone/scripts/train.py
+++ capstone/scripts/train.py
@@ -7,57 +7,20 @@ import joblib
 from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
+from azureml.core import Workspace
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 # import requests
 # import io
 
-# Data is located at:
-# url = ""
-# dataset = TabularDatasetFactory.from_delimited_files(url)
-
-# This other approach creates a DataFrame from the data
-# ds = requests.get(url, allow_redirects=True)
-# df = pd.read_csv(io.StringIO(ds.content.decode('utf-8')))
-
-# Stored locally
-df = pd.read_csv('../data/today.csv')
-
 # Connect to workspace
 ws = Workspace.from_config()
 datastore = ws.get_default_datastore()
 
-# Register dataset
-ds = TabularDatasetFactory.register_pandas_dataframe(df, target=[(datastore, './training')], name='dataframe_name')
-
+dataset = get(ws, name='hd-dataset')
 
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
-    # x_df = data.dropna()
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-
+    y_df = x_df.pop("y_c_shift")
     return x_df, y_df
 
 # Clean data and split for training and testing
