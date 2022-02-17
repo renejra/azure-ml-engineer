b3cb4b47539c362a3ff580f3754209ec50e0c063
renejra <rjriveroarrieta@gmail.com>
2022-02-17 11:24:59 +0100

    changes done

--- capstone/scripts/train.py
+++ capstone/scripts/train.py
@@ -2,34 +2,36 @@ from sklearn.linear_model import LogisticRegression
 import argparse
 import joblib
 from sklearn.model_selection import train_test_split
-from azureml.core import Workspace, Dataset
-from azureml.core.run import Run
+from azureml.core import Workspace, Dataset, Run
 import os
 # from pathlib import Path
 
-ws = Workspace.from_config()
-# datastore = ws.get_default_datastore()
-dataset = Dataset.get_by_name(workspace=ws, name='hd_dataset')
-df = dataset.to_pandas_dataframe()
 
-def clean_data(x):
+def clean_data(dataset):
+    x = dataset.to_pandas_dataframe()
     y = x.pop("y_c_shift")
     return x, y
 
-run = Run.get_context()
-x, y = clean_data(df)
-x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, train_size=0.75)
-print(f'training shape: {x_train.shape}, labels: {y_train.shape} \ntesting shape: {x_test.shape}, labels: {y_test.shape}')
-
-
 def main():
+    # Get workspace details
+    run = Run.get_context()
+    ws = run.experiment.workspace
+    # ws = Workspace.from_config()
+    # datastore = ws.get_default_datastore()
+
     # Parse hyperparameter as arguments for training script
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--C', type=float, default=1.0,
                         help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument("--ds_name", type=str, default='hd-dataset')
     args = parser.parse_args()
+
+    # clean the data
+    dataset = Dataset.get_by_name(workspace=ws, name=args.ds_name)
+    x, y = clean_data(dataset)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, train_size=0.75)
+    print(f'training shape: {x_train.shape}, labels: {y_train.shape} \ntesting shape: {x_test.shape}, labels: {y_test.shape}')
 
     # run logs
     run.log("regularization strength:", float(args.C))
